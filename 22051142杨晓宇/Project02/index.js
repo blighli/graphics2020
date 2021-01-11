9964e371c1f95f297132edee272516faae2422e1
杨晓宇 <1329218916@qq.com>
2021-01-11 17:41:45 +0800

    Project02
    使用着色器实现光照效果

--- 22051142杨晓宇/Project02/index.js
+++ 22051142杨晓宇/Project02/index.js
@@ -6,21 +6,106 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setClearColor(new THREE.Color(0x000000));
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
+/* 摄像机 */
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 300, 0);
+camera.lookAt(scene.position);
+document.getElementById("webgl-output").appendChild(renderer.domElement);
 /* 网状平面 */
 const gridsize = 1000;
 const divisions = 25;
 const gridHelper = new THREE.GridHelper(gridsize, divisions, new THREE.Color(0x00ff00), new THREE.Color(0x00ff00));
 scene.add(gridHelper);
+
+/* 纹理加载器 */
+const textureLoader = new THREE.TextureLoader();
+/* 着色器 */
+const vertexShader =
+    `
+out vec3 fColor;
+varying vec2 fTexCoord;
+uniform vec4 lightPosition; // 光源位置
+uniform vec3 ambientLightIntensity, diffuseLightIntensity, specularLightIntensity;
+uniform vec3 ambientReflectivity, diffuseReflectivity, specularReflectivity;
+uniform float shininess;
+void main(void) {
+    vec4 light = viewMatrix * lightPosition;
+    vec3 N = normalize( normalMatrix * normal);
+    vec4 pos = modelViewMatrix * vec4(position,1.0);
+    vec3 s = normalize(vec3(light - pos));
+    vec3 v = normalize(-pos.xyz);
+    vec3 r = reflect( -s, N );
+    float sDotN = max( dot(s, N), 0.0 );
+
+    vec3 ambient = ambientLightIntensity * ambientReflectivity;
+    vec3 diffuse = sDotN * diffuseLightIntensity * diffuseReflectivity;
+    vec3 specular = vec3(0.0);
+
+    if(sDotN < 0.0){
+        specular = specularLightIntensity * specularReflectivity *
+              pow( max( dot(r,v), 0.0 ), shininess );
+    }
+
+    fColor = ambient + diffuse + specular;
+    fTexCoord = uv;
+
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}`
+const fragmentShader = `
+in vec3 fColor;
+varying vec2 fTexCoord;
+uniform sampler2D map;
+void main(void)
+{
+    vec4 texel = texture2D(map,fTexCoord);
+    vec3 tc = texel.rgb;
+    float ta = texel.a;
+    gl_FragColor = vec4(fColor * tc,ta);
+}`
+
 /* 太阳 */
 const sunGrometry = new THREE.SphereGeometry(20, 20, 20);//球体半径
-const sunMaterial = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("./img/sun.jpg") });
+let lightPosition = new THREE.Vector4(0., 0., 0., 1.);
+const sunMaterial = new THREE.ShaderMaterial({
+
+    uniforms: {
+        'lightPosition': { value: lightPosition },
+        'ambientLightIntensity': { value: new THREE.Vector3(1., 1., 1.) },//环境光强度
+        'diffuseLightIntensity': { value: new THREE.Vector3(1., 1., 1.) },//漫反射光强度
+        'specularLightIntensity': { value: new THREE.Vector3(1., 1., 1.) },//镜面反射光强度
+        'ambientReflectivity': { value: new THREE.Vector3(1., 1., 1.) },//环境光反射系数
+        'diffuseReflectivity': { value: new THREE.Vector3(1., 1., 1.) },//漫反射系数
+        'specularReflectivity': { value: new THREE.Vector3(1., 1., 1.) },//镜面反射系数
+        'map': { value: textureLoader.load("./img/sun.jpg") },
+        'shininess': { value: 1.0 }
+    },
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+
+});
 const sun = new THREE.Mesh(sunGrometry, sunMaterial);
 sun.position.set(0, 0, 0);
 
 /* 地球 */
 const earthRevolutionRadius = 100;//公转半径
 const earthGrometry = new THREE.SphereGeometry(10, 20, 20);//球体半径
-const earthMaterial = new THREE.MeshLambertMaterial({ map: new THREE.TextureLoader().load("./img/earth.jpg") });
+const earthMaterial = new THREE.ShaderMaterial({
+
+    uniforms: {
+        'lightPosition': { value: lightPosition },
+        'ambientLightIntensity': { value: new THREE.Vector3(0., 0., 0.) },//环境光强度
+        'diffuseLightIntensity': { value: new THREE.Vector3(1., 1., 1.) },//漫反射光强度
+        'specularLightIntensity': { value: new THREE.Vector3(1., 1., 1.) },//镜面反射光强度
+        'ambientReflectivity': { value: new THREE.Vector3(1., 1., 1.) },//环境光反射系数
+        'diffuseReflectivity': { value: new THREE.Vector3(1., 1., 1.) },//漫反射系数
+        'specularReflectivity': { value: new THREE.Vector3(1., 1., 1.) },//镜面反射系数
+        'map': { value: textureLoader.load("./img/earth.jpg") },
+        'shininess': { value: 10 }
+    },
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+
+});
 const earth = new THREE.Mesh(earthGrometry, earthMaterial);
 earth.receiveShadow = true;
 earth.castShadow = true;
@@ -29,7 +114,23 @@ earth.position.set(earthRevolutionRadius, 0, 0);
 /* 月球 */
 const moonRevolutionRadius = 25;//公转半径
 const moonGrometry = new THREE.SphereGeometry(4, 20, 20);//球体半径
-const moonMaterial = new THREE.MeshLambertMaterial({ map: new THREE.TextureLoader().load("./img/moon.jpg") });
+const moonMaterial = new THREE.ShaderMaterial({
+
+    uniforms: {
+        'lightPosition': { value: lightPosition },
+        'ambientLightIntensity': { value: new THREE.Vector3(0., 0., 0.) },//环境光强度
+        'diffuseLightIntensity': { value: new THREE.Vector3(1., 1., 1.) },//漫反射光强度
+        'specularLightIntensity': { value: new THREE.Vector3(1., 1., 1.) },//镜面反射光强度
+        'ambientReflectivity': { value: new THREE.Vector3(1., 1., 1.) },//环境光反射系数
+        'diffuseReflectivity': { value: new THREE.Vector3(1., 1., 1.) },//漫反射系数
+        'specularReflectivity': { value: new THREE.Vector3(1., 1., 1.) },//镜面反射系数
+        'map': { value: textureLoader.load("./img/moon.jpg") },
+        'shininess': { value: 0.1 }
+    },
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+
+});
 const moon = new THREE.Mesh(moonGrometry, moonMaterial);
 moon.receiveShadow = true;
 moon.castShadow = true;
@@ -58,15 +159,6 @@ function moonRevolution() {
     moonAngle += 0.6;
     if (moonAngle >= 360) moonAngle -= 360;
 }
-/* 光源 */
-const sunlight = new THREE.PointLight("#ffffff");
-sunlight.position.set(0, 0, 0);
-scene.add(sunlight);
-/* 摄像机 */
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 300, 0);
-camera.lookAt(scene.position);
-document.getElementById("webgl-output").appendChild(renderer.domElement);
 /* 轨道控制器 */
 const controls = new OrbitControls(camera, renderer.domElement);
 /* 点击事件 */
